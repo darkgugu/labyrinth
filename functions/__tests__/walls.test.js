41335e3bf1309620429254a28027e3fab02d4afa
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-11-08 03:23:03 +0100

    Implémentation de walls

--- functions/__tests__/walls.test.js
+++ functions/__tests__/walls.test.js
@@ -1,6 +1,6 @@
 const walls = require('../walls')
 
-describe('Tests de la fonction getWalls', () => {
+describe('Tests de walls', () => {
     it('Retourne un objet contenant un tableau vertical et un tableau horizontal', () => {
         expect(walls).toHaveProperty('vertical');
         expect(walls).toHaveProperty('horizontal');
---
+++ functions/walls.js
@@ -0,0 +1,5 @@
+let walls = {
+    vertical: [],
+    horizontal: []
+}
+module.exports = walls;
