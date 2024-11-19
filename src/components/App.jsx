b1325f3f05047a03c60343a64607f2ee5502a568
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-11-19 19:37:10 +0100

    Create App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import '../assets/App.css';
+import { GameArea } from './GameArea';
+
+
+function App() {
+  return (
+    <div className="App">
+      <GameArea />
+    </div>
+  );
+}
+
+export default App;
