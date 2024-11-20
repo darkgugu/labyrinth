42e2bc9406246f066785c9dc9732d6774b1a8dcc
Icham Duret <icham.duret14@gmail.com>
2024-11-20 09:09:41 +0100

    Fix apiCall import

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 const functions = require("firebase-functions/v1");
-const callApi = require('./callApi')
+const { callApi } = require('./callApi')
 
 exports.dailyApiCall = functions.pubsub
   .schedule("0 0 * * *")
-  .timeZone("UTC")
+  .timeZone("Europe/Paris")
   .onRun(async (context) => {
     return await callApi();
   });
