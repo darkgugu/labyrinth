74ba5deee14c7f8fc9a6643efa81ed1cbf646476
Icham Duret <icham.duret14@gmail.com>
2024-11-20 10:17:21 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions/v1");
 const { callApi } = require('./callApi')
 
 exports.dailyApiCall = functions.pubsub
-  .schedule("* * * * *")
+  .schedule("0 0 * * *")
   .timeZone("Europe/Paris")
   .onRun(async (context) => {
     return await callApi();
