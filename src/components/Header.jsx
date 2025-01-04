b13ca519d012efdd74af2f0512023194d3f113dd
Icham Duret <icham.duret14@gmail.com>
2025-01-04 18:41:57 +0100

    Added Header.jsx unit test.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
+import React from 'react'
 import '../assets/css/Header.css'
 
 export const Header = () => {
 	return (
-		<div className="Header">
+		<div className="Header" data-testid="header">
 			<h1>Labyrinth</h1>
 		</div>
 	)
